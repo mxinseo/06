25dffc43ceab545a2a8ef31f5714d7e362276fc3
rudalstj <jade201913@gmail.com>
2022-10-11 16:45:49 +0900

    #2 수정

--- main.c
+++ main.c
@@ -3,16 +3,26 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void func(int x)
-{	
+void func2(void)
+{
+	int x;
+	
+	printf("func2 x is at %p\n", &x);
+}
+
+void func1(void)
+{
+	int x;
+	
 	printf("func1 x is at %p\n", &x);
+	func2();
 }
 
 int main(int argc, char *argv[]) {
 	int x;
 	
 	printf("main x is at %p\n", &x);
-	func(x);
+	func1();
 
 	return 0;
 }
